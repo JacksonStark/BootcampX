f134fad85e85379c8087e8f4b3b46ce03d3bf4ab
Jackson Stark <jacksonstark77@hotmail.com>
2019-10-16 23:52:31 +0000

    add database connection and test queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -11,15 +11,18 @@ let cohortMonth = process.argv[2];
 let maxResults = process.argv[3];
 
 pool.query (`
-SELECT students.id as student_id, students.name as student_name, cohort_id, cohorts.name as cohort_name
-FROM students
+SELECT DISTINCT teachers.name as teacher_name, cohorts.name as cohort_name
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
 WHERE cohorts.name LIKE '%${cohortMonth}%'
+ORDER BY teacher_name
 LIMIT ${maxResults || 5};
 `)
 .then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.student_name} has an id of ${user.student_id} and was in the ${user.cohort_name} cohort.`)
+  res.rows.forEach(teacher => {
+    console.log(`${teacher.cohort_name}: ${teacher.teacher_name}`)
   })
 })
 .catch(err => console.log('query error:', err.stack));
